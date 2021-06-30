861483818ad0da96a895ae268dbfcca72f4cd94e
mhggpo <mhggpo@hotmail.com>
2021-06-30 16:35:25 +0800

    Add ReportDao(partly)

--- src/main/java/com/zero/dao/ReportDao.java
+++ src/main/java/com/zero/dao/ReportDao.java
@@ -9,17 +9,19 @@ public interface ReportDao {
      * 预定客人报表
      * @param min 起始日期
      * @param max 结束日期
+     * @param roomId 客房编号
      * @return 返回操作条数
      */
-    int selectReserveMember(@Param("min") Date min,@Param("max") Date max);
+    int selectReserveMember(@Param("min") Date min,@Param("max") Date max,@Param("roomId") Integer roomId);
 
     /**
      * 入住客人报表
      * @param min 起始日期
      * @param max 结束日期
+     * @param roomId 客房编号
      * @return 返回操作条数
      */
-    int selectCheckInMember(@Param("min") Date min,@Param("max") Date max);
+    int selectCheckInMember(@Param("min") Date min,@Param("max") Date max,@Param("roomId") Integer roomId);
 
     /**
      * 当日预定客人报表
@@ -39,9 +41,10 @@ public interface ReportDao {
      */
     int selectCheckOutMemberToday();
 
-    /**
-     * 当日财务进账报表
-     * @return 返回操作条数
-     */
-    int selectFinanceToday();
+    //这里要改数据库
+//    /**
+//     * 当日财务进账报表
+//     * @return 返回操作条数
+//     */
+//    int selectFinanceToday();
 }
