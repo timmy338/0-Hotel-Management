9206e05642835d60feabdf43f028329574949fff
Ending <984459598@qq.com>
2021-06-30 17:18:10 +0800

    增加CheckInDao.xml 修改CheckInDao.java

--- src/main/java/com/zero/dao/CheckInDao.java
+++ src/main/java/com/zero/dao/CheckInDao.java
@@ -6,14 +6,19 @@ import org.apache.ibatis.annotations.Param;
 import java.util.List;
 
 public interface CheckInDao {
+    /**
+     * 通过编号查询客房
+     * @param roomId 客房编号
+     * @return 返回查询到的客房列表
+     */
     List<Room> selectRoomById(@Param("roomId")Integer roomId);
 
     /**
      *通过类型查询客房
-     * @param roomType 客房类型
+     * @param roomType 客房类型名
      * @return 返回查询到的客房列表
      */
-    List<Room> selectRoomByType(RoomType roomType);
+    List<Room> selectRoomByType(@Param("roomType") String roomType);
 
     /**
      *通过状态查询客房
@@ -24,10 +29,10 @@ public interface CheckInDao {
 
     /**
      *通过楼层查询客房
-     * @param floor 楼层对象
+     * @param floor 楼层名
      * @return 返回查询到的客房列表
      */
-    List<Room> selectRoomByFloor(Floor floor);
+    List<Room> selectRoomByFloor(@Param("floor") String floor);
 
     /**
      *通过价格查询客房
@@ -37,18 +42,11 @@ public interface CheckInDao {
     List<Room> selectRoomByPrice(@Param("roomPrice")double roomPrice);
 
     /**
-     *查询客房
-     * @param room 客房对象
-     * @return 返回查询到的客房列表
-     */
-    List<Room> selectRoom(Room room);
-
-    /**
      * 填写入住信息
      * @param checkIn 入住对象
      * @return 返回0表示失败 返回正整数表示数据库中受影响的条数
      */
-    int fillCheckIn(CheckIn checkIn);
+    int insertCheckIn(CheckIn checkIn);
 
 //    /**
 //     * 打印入住单
@@ -57,12 +55,12 @@ public interface CheckInDao {
 //     */
 //    int printCheckIn(CheckIn checkIn);
 
-    /**
-     * 保存登记信息
-     * @param checkIn 入住对象
-     * @return 返回0表示失败 返回正整数表示数据库中受影响的条数
-     */
-    int saveCheckIn(CheckIn checkIn);
+//    /**
+//     * 保存登记信息
+//     * @param checkIn 入住对象
+//     * @return 返回0表示失败 返回正整数表示数据库中受影响的条数
+//     */
+//    int saveCheckIn(CheckIn checkIn);
 
     /**
      *根据入住单号查询入住客房信息
@@ -87,23 +85,23 @@ public interface CheckInDao {
 
     /**
      *根据入住人查询入住客房信息
-     * @param user 入住人对象
+     * @param guest 入住人名字
      * @return 返回查询到的客房信息列表
      */
-    List<CheckIn> selectCheckInByUser(@Param("user") Integer user);
+    List<CheckIn> selectCheckInByUser(@Param("guest") String guest);
 
-    /**
-     * 预定转入住
-     * @param reserve 预定对象
-     * @return 入住后的CheckIn列表
-     * */
-    List<CheckIn> reserveToCheckIn(Reserve reserve);
+//    /**
+//     * 预定转入住
+//     * @param id 预定id
+//     * @return 入住后的CheckIn列表
+//     * */
+//    List<CheckIn> reserveToCheckIn(@Param("id") Integer id);
 
     /**
      * 换房
-     * @param checkIn 需要更改的CheckIn对象
-     * @param room 更改后的room
+     * @param checkInId 需要更改的CheckInId
+     * @param roomId 更改后的roomId
      * @return 返回0表示失败 返回正整数表示数据库中受影响的条数
      */
-    int changeRoom(CheckIn checkIn,Room room);
+    int changeRoom(@Param("checkInId") Integer checkInId,@Param("roomId") Integer roomId);
 }
