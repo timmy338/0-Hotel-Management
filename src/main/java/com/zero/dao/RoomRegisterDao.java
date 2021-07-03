c520e508ae580e324ebbf56706fdd675ef44116d
Ending <984459598@qq.com>
2021-07-03 16:36:09 +0800

    整了个RoomRegister的insert

--- src/main/java/com/zero/dao/RoomRegisterDao.java
+++ src/main/java/com/zero/dao/RoomRegisterDao.java
@@ -1,11 +1,19 @@
 package com.zero.dao;
 
+import com.zero.pojo.Reserve;
 import com.zero.pojo.RoomRegister;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface RoomRegisterDao {
+    /**
+     * 增加RoomRegister
+     * @param roomRegister RoomRegister对象
+     * @return 返回0表示失败 返回正整数表示数据库中受影响的条数
+     */
+    int insertRoomRegister(RoomRegister roomRegister);
+
     /**
      *通过id查询RoomRegister
      * @param id RoomRegister的id
